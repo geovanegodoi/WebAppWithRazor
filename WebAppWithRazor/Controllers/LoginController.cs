f8727467f6a13c4ef1516177627d102a749569b9
Geovane Godoi <geovane.godoi@gmail.com>
2019-01-29 22:27:36 -0200

    Commits the initial release

---
+++ WebAppWithRazor/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using WebAppWithRazor.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppWithRazor.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public LoginController(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate(UserCredentialsModel model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserCredentialsModel model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebAppWithRazor/Models/UserCredentialsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebAppWithRazor.Models
+{
+    public class UserCredentialsModel
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}

47669044cb488aecb1b79bdddb7c8cd5450d333c
Geovane Godoi <geovane.godoi@gmail.com>
2019-02-04 22:36:56 -0200

    Commits the _BaseCrudPage

---
+++ WebAppWithRazor/Models/LayoutModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebAppWithRazor.Models
+{
+    public static class LayoutModel
+    {
+        public static readonly string BasePage = "_BasePage";
+
+        public static readonly string BaseLayoutPage = "_BaseLayoutPage";
+
+        public static readonly string BaseCrudPage = "_BaseCrudPage";
+    }
+}
